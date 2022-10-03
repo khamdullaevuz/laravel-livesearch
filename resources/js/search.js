1337f9a2fc98f667e57fafa4a1355c597e8eb742
khamdullaevuz <elbek.khamdullaev@gmail.com>
2022-10-03 15:55:27 +0500

    axios updates

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,7 +1,7 @@
 document.querySelector('#search').addEventListener('keyup', function() {
     axios.post('/search', {
         search_text: document.getElementById('search').value
-    }).then(function (response) {
-        document.getElementById('persons-data').innerHTML = response.data;
-    });
+    })
+        .then( response => document.getElementById('persons-data').innerHTML = response.data)
+        .catch( error => console.log(error) );
 });
